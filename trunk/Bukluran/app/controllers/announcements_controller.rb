92f8a143e7a7031bfb992ca5ed1ee6f25ed3273e
samaelsamuel <samaelsamuel@5f1ee6d6-0ea9-11de-a22a-7557bf72ca44>
2009-04-02 15:12:53 +0000

    eto yung may uploads na ni bonn. debugged.

--- trunk/Bukluran/app/controllers/announcements_controller.rb
+++ trunk/Bukluran/app/controllers/announcements_controller.rb
@@ -1,10 +1,10 @@
 class AnnouncementsController < ApplicationController
   def new
-    @announcements = Osa.find(session[:user_id]).announcements
+    @announcements = Osa.find_by_account_id(session[:user_id]).announcements
   end
 
   def create
-    @announcement = Osa.find(session[:user_id]).announcements.new(params[:announcement])
+    @announcement = Osa.find_by_account_id(session[:user_id]).announcements.new(params[:announcement])
     if @announcement.save
       redirect_to :controller => 'osas', :action => 'index'
     else
